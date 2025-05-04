3456a5ca4ebaeb6b0e98635735df2729f82444f7
rambhardwajj <rambhardwaj101000@gmail.com>
2025-05-04 22:53:38 +0530

    fix: generated file ignored

--- BackEnd/src/generated/prisma/edge.js
+++ BackEnd/src/generated/prisma/edge.js
@@ -125,7 +125,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
